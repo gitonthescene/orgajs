4d633b3aedc1682889c1fbaa7f79d0ce98f21136
Douglas Mennella <douglas.mennella@gmail.com>
2020-05-29 11:41:03 +0900

    Cleaner handling of successive blank tokens

--- packages/orga/src/processors/blank.ts
+++ packages/orga/src/processors/blank.ts
@@ -2,18 +2,18 @@ import Node from '../node'
 
 function _process(token, section) {
   const self = this
-  self._cel++
-  var blank = self.consume()
+
+  // @@DAM how do we reach the end of the stream?
+  var blankSpace = '';
+  var blank = token
+  while(blank && blank.name === `blank`) {
+    if (token.raw === `\n`) self._cel++
+    blankSpace += blank.raw;
+    self.consume();
+    blank = self.peek();
+  }
   if (section.type === `footnote.definition` && self._cel > 1) return section
   self._aks = {}
-  // @@DAM how do we reach the end of the stream?
-  var blankSpace = blank && blank.raw;
-  blank = self.peek()
-  while(blank && blank.name === `blank`) {
-      blankSpace += blank.raw;
-      self.consume();
-      blank = self.peek();
-  }
   section.push(new Node('whitespace').with({ value: blankSpace }));
   return self.parseSection(section)
 }
