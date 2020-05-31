9fa251d92eec2c4f44172306e691fef353888fe2
Douglas Mennella <douglas.mennella@gmail.com>
2020-05-31 12:36:29 +0900

    Handle newlines properly now that tree is more accurate

--- packages/orga-stringify/src/list.js
+++ packages/orga-stringify/src/list.js
@@ -9,14 +9,17 @@ function serializeText(ctx, node, index, parent) {
       ? { ...ctx, level: (ctx.level === undefined ? -1 : ctx.level) + 1 }
       : { ...ctx };
   var orig = all(newctxt, node);
-  var inner = orig.replace(/\r\n|\n$/, "").split(/\r\n|\n/);
+  var inner = orig ? orig.replace(/\r\n|\n$/, "").split(/\r\n|\n/) : [];
   var indent = "";
   if (node.type == "list") {
     indent = ctx.level === undefined ? "" : "  ";
-    return indent + inner.join(`\n${indent}`) + "\n";
+    var ret = inner ? indent + inner.join(`\n${indent}`) + "\n" : "";
+    return ret;
   } else {
-    indent = "  ";
-    inner = indent + inner.join(`\n${indent}`) + "\n";
+    indent = ctx.level === 0 ? "" : "  ";
+    if (inner.length > 0) {
+      inner = indent + inner.join(`\n${indent}`) + "\n";
+    } else inner = "";
   }
   var checked =
     node.checked === true ? "[X] " : node.checked == false ? "[ ] " : "";
