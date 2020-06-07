862ff8faa0045640889c62e0c5583f00ea4d63a2
Douglas Mennella <douglas.mennella@gmail.com>
2020-06-07 15:38:38 +0900

    Body of block now in a subtree

--- packages/orga-stringify/src/handlers/block.js
+++ packages/orga-stringify/src/handlers/block.js
@@ -7,5 +7,5 @@ function serializeText(ctx, node, index, parent) {
   var orig = all(ctx, node);
   var params = node.params.join(",");
   params = params.length ? ` ${params}` : "";
-  return `#+BEGIN_${node.name}${params}\n${node.value}#+END_${node.name}\n`;
+  return `#+BEGIN_${node.name}${params}\n${orig}#+END_${node.name}\n`;
 }
